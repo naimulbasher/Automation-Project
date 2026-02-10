847ce717879d11abf189e277f6e59b094c3c4c7f
naimulbasher <naimulbasher122@gmail.com>
2026-02-10 16:15:14 +0600

    utils added

---
+++ utils/envConfigQ1.js
@@ -0,0 +1,3 @@
+export const BASE_URL = "https://www.saucedemo.com/";
+export const USERNAME = "locked_out_user";
+export const PASSWORD = "secret_sauce";
---
+++ utils/envConfigQ2.js
@@ -0,0 +1,3 @@
+export const BASE_URL = "https://www.saucedemo.com/";
+export const USERNAME = "standard_user";
+export const PASSWORD = "secret_sauce";
